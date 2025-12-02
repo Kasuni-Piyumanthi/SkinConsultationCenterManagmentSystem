69b3f497b46c079d514ec0ae08773b3c442ad56b
Kasuni-Piyumanthi <kpiyumanthi@gmail.com>
2025-12-02 23:23:29 +0530

    added methods

--- Doctor.java
+++ Doctor.java
@@ -1,2 +1,7 @@
-public class Doctor {
+public class Doctor extends Person{
+
+    private int medicalLicenceNumber;
+    private String specialisation;
+
+    
 }
--- Person.java
+++ Person.java
@@ -1,2 +1,9 @@
+import java.time.LocalDate;
+
 public class Person {
+
+    private String name;
+    private String surname;
+    private LocalDate dateOfBirth;
+    private int mobileNumber;
 }
